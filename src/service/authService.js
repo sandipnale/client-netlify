54baea5bcf57bcd3c776567baec56bed01c24bb1
sandipnale <sandipnale143sa@gmail.com>
2025-07-31 12:02:52 +0530

    Update authService.js

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://ec2-13-126-78-224.ap-south-1.compute.amazonaws.com:8080/api";
 
 export const registerUser = async (data) => {
     try {
